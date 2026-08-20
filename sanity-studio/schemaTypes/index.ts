b7dc2c9183ace487e3d6265c00d9626dd557a033
kushagra0328 <kushagra.agrawal500@gmail.com>
2026-08-20 20:06:31 +0530

    Register website content and leadership schemas

--- sanity-studio/schemaTypes/index.ts
+++ sanity-studio/schemaTypes/index.ts
@@ -1,4 +1,6 @@
 import {investorDisclosure} from './investorDisclosure'
 import {governanceDocument} from './governanceDocument'
+import {siteSection} from './siteSection'
+import {managementProfile} from './managementProfile'
 
-export const schemaTypes = [investorDisclosure, governanceDocument]
+export const schemaTypes = [investorDisclosure, governanceDocument, siteSection, managementProfile]
